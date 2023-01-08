b4467d7035c7fd4ecf797ed745df17c6446ba303
lala lala <lucas.a.leray@gmail.com>
2023-01-08 12:08:16 +0100

    Test basic tokenizer

--- src/tokenization/tokenizers/basic.rs
+++ src/tokenization/tokenizers/basic.rs
@@ -48,7 +48,7 @@ impl BasicTokenizer {
 impl Tokenizer for BasicTokenizer {
     fn fit_(&mut self, corpus: &str) {
         let tokens: Vec<Token> = self.pre_process(corpus);
-        self.vocabulary = self.extract_vocabulary(&tokens);
+        self.vocabulary.extend(self.extract_vocabulary(&tokens));
     }
 
     fn transform_(&self, corpus: &str) -> Vec<Token> {
@@ -79,5 +79,66 @@ impl BasicTokenizer {
     #[getter]
     fn vocabulary(&self) -> PyResult<Vec<Word>> {
         Ok(self.vocabulary.to_vec())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::tokenization::pre_tokenizers::PreTokenizerKind;
+
+    #[test]
+    fn fitted_vocabulary() {
+        let mut tokenizer = BasicTokenizer::new_(
+            None, None
+        );
+
+        tokenizer.fit("Hello World!");
+        assert_eq!(tokenizer.vocabulary, vec!["Hello World!"]);
+    }
+
+    #[test]
+    fn custom_vocabulary() {
+        let mut tokenizer = BasicTokenizer::new_(
+            Some(&vec!["Lucas".to_string()]), None
+        );
+
+        tokenizer.fit("Hello World!");
+        assert_eq!(tokenizer.vocabulary, vec!["Lucas", "Hello World!"]);
+    }
+
+    #[test]
+    fn pre_tokenizers_are_applied() {
+        let mut tokenizer = BasicTokenizer::new_(
+            None,
+            Some(&vec![PreTokenizerKind::WhiteSpace])
+        );
+
+        tokenizer.fit("Hello World!");
+        assert_eq!(tokenizer.vocabulary, vec!["Hello", "World!"]);
+    }
+
+    #[test]
+    fn transformation() {
+        use crate::tokenization::special_tokens::UNK;
+
+        let mut tokenizer = BasicTokenizer::new_(
+            None,
+            Some(&vec![
+                PreTokenizerKind::WhiteSpace,
+                PreTokenizerKind::Punctuation,
+            ])
+        );
+
+        tokenizer.fit("Hello World!");
+        let result: Vec<Token> = tokenizer.transform("Hello Lucas!");
+        assert_eq!(
+            result,
+            vec![
+                Token{word: "Hello".to_string()},
+                Token{word: UNK.to_string()},
+                Token{word: "!".to_string()},
+            ]
+        );
     }
 }
